defa99d2f0400682d1257111842eb1087c1cd44f
lzylzy2025 <1841797371@qq.com>
2026-05-31 20:50:00 +0800

    mapper修改了MySQL语法

--- weibo-server/src/main/java/com/example/weiboserver/mapper/WeiboMapper.java
+++ weibo-server/src/main/java/com/example/weiboserver/mapper/WeiboMapper.java
@@ -2,6 +2,7 @@ package com.example.weiboserver.mapper;
 
 import com.example.weiboserver.bean.Weibo;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
@@ -9,6 +10,7 @@ public interface WeiboMapper {
     @Select("SELECT id, content, create_time as createTime, user_id as userId FROM weibo ORDER BY create_time DESC")
     List<Weibo> findAll();
 
-    @Insert("INSERT INTO weibo(content, use_id) VALUES(#{content}, #{userid})")
-    int addWeibo(String weibo, long userid);
+    @Insert("INSERT INTO weibo(content, user_id) VALUES(#{weibo}, #{userid})")
+    int addWeibo(@Param("weibo") String weibo,
+                 @Param("userid") long userid);
 }
